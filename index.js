da2dd96e5bb7871946abe98530ae798dd42b99de
Marten Rebane <marten@Martens-MacBook-Pro.local>
2015-06-03 14:15:19 +0300

    Google PassPort

--- index.js
+++ index.js
@@ -1,17 +1,83 @@
 // Server (aka server.js)
 
+var config = require('./config/config');
+
 var express = require('express');
 var mongoose = require('mongoose');
 var passport = require('passport');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
-var app = express();
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
-app.get('/api/hello', function(req, res) {
-  console.log('Somebody visited /api/hello');
-  res.json({hello:'hello'});
+passport.use(new GoogleStrategy({
+
+    callbackURL: config.googleAuth.callbackURL,
+    realm: config.realm,
+    clientSecret: config.googleAuth.clientSecret,
+    clientID: config.googleAuth.clientID
+  },
+  function(accessToken, refreshToken, profile, done) {
+    console.log(profile);
+    done(null, profile);
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
 });
 
-var server = app.listen(3000, function() {
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+var sessionOpt = {
+  secret: config.secret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {httpOnly: true, maxage: 2419200000}
+};
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser(config.secret));
+app.use(session(sessionOpt));
+app.use(passport.initialize());
+app.use(passport.session());
+
+/* Google Authentication */
+app.get('/api/auth/google',
+  passport.authenticate('google', { scope: [
+    'https://www.googleapis.com/auth/userinfo.profile',
+    'https://www.googleapis.com/auth/userinfo.email'
+    ]}));
+
+app.get('/api/oauth2callback',
+  passport.authenticate('google', { failureRedirect: '/#/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/#/');
+  });
+
+
+  var auth = function(req, res, next) {
+    if(!req.isAuthenticated()) {
+      res.status(401).send({error: 'Unauthorized'});
+    }
+
+    else {
+      next();
+    }
+  }
+
+  app.get('/api/me', auth, function(req, res) {
+    return res.json(req.session.passport.user);
+  });
+
+var server = app.listen(config.port, function() {
   var host = server.address().address;
   var port = server.address().port;
 
