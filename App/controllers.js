cec8de3672c82895fe6d4f58a9adc8a794d7d446
Andre <andrep@tlu.ee>
2015-06-03 14:56:27 +0300

    Updated controllers
    controllers.js
    index.html
    home.html

--- App/controllers.js
+++ App/controllers.js
@@ -1,7 +1,24 @@
 var leplannerControllers = angular.module('leplannerControllers', []);
+
+leplannerControllers.controller('MainCtrl', [
+  '$scope',
+  '$http',
+  function($scope, $http){
+    // user auth
+    $http.get('/api/me').
+    success(function(data, status, headers, config){
+      console.log('User logged in');
+      $scope.user = data.displayName;
+    }).
+    error(function(data, status, headers, config){
+      console.log(data);
+    });
+  }
+]);
+
 leplannerControllers.controller('homeCtrl', [
   '$scope',
   function($scope){
-    $scope.message = 'tere';
+    $scope.message = 'Tere';
   }
 ]);
