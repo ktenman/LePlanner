2b4fa3e0c52934faf39c5a02280d4c7d8ebadc88
romilrobtsenkov <romilrobtsenkov@Romils-MacBook-Air.local>
2015-06-03 14:03:18 +0300

    added passport auth

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var config = require('./config/config');
 var express = require('express');
 var mongoose = require('mongoose');
 var passport = require('passport');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-
 passport.use(new GoogleStrategy({
     callbackURL: config.googleAuth.callbackURL,
     realm: config.realm,
@@ -14,17 +16,50 @@ passport.use(new GoogleStrategy({
   },
   function(accessToken, refreshToken, profile, done) {
     console.log(profile);
+
+    done(null,profile);
   }
 ));
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
 
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+var sessionOpt = {
+  secret: config.secret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {httpOnly: true, maxAge: 2419200000}
+};
 
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cookieParser(config.secret));
+app.use(session(sessionOpt));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/api/hello', function(req, res){
-  console.log('somebody visited /api/hello');
-  res.json({hello:'hello'});
-});
+
+/* GOOGLE AUTH */
+app.get('/api/auth/google',
+  passport.authenticate('google', { scope: [
+    'https://www.googleapis.com/auth/userinfo.profile',
+    'https://www.googleapis.com/auth/userinfo.email'
+  ]}));
+
+app.get('/api/oauth2callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/#/');
+  });
+
+
 
 var server = app.listen(config.port, function(){
 
