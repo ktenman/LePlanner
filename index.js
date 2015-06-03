d51b079e59f5d895e8bb4d11b2329428f2a26555
romilrobtsenkov <romilrobtsenkov@Romils-MacBook-Air.local>
2015-06-03 13:39:16 +0300

    added google OAuth2Strategy

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+var config = require('./config/config');
+
 var express = require('express');
 var mongoose = require('mongoose');
 var passport = require('passport');
+
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+passport.use(new GoogleStrategy({
+    callbackURL: config.googleAuth.callbackURL,
+    realm: config.realm,
+    clientSecret: config.googleAuth.clientSecret,
+    clientID: config.googleAuth.clientID
+  },
+  function(accessToken, refreshToken, profile, done) {
+    console.log(profile);
+  }
+));
+
+
 
 var app = express();
 
@@ -9,7 +26,7 @@ app.get('/api/hello', function(req, res){
   res.json({hello:'hello'});
 });
 
-var server = app.listen(3000, function(){
+var server = app.listen(config.port, function(){
 
   var host = server.address().address;
   var port = server.address().port;
