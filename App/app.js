af4f2a928787da323c32a95dad58a1454e8862d5
Andre <andrep@tlu.ee>
2015-06-03 13:07:00 +0300

    Angular-resource and Angular-route added
    home.html
    login.html

---
+++ App/app.js
@@ -0,0 +1,12 @@
+var leplannerApp = angular.module('leplannerApp', [
+  'ngResource', 'ngRoute'
+]);
+
+leplannerApp.config(['$routeProvider', '$locationProvider', '$resourceProvider',
+function($routeProvider, $locationProvider, $resourceProvider){
+  $routeProvider
+  .when('/', {templateUrl:'/views/home.html'})
+  .when('/login', {templateUrl:'/views/login.html'})
+  .otherwise({redirectTo:'/'});
+
+}]);
