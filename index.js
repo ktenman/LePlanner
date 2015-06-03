60410f4af4e3f46916b3296ef2958e632c002603
vlad987 <minajev@tlu.ee>
2015-06-03 14:06:03 +0300

    added passport auth

--- index.js
+++ index.js
@@ -1,15 +1,62 @@
 // server
+var config = require('./config/config');
 var express = require('express');
 var mongoose = require('mongoose');
 var passport = require('passport');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
+
+//GOOGLE AUTH https://github.com/jaredhanson/passport-google-oauth
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+passport.use(new GoogleStrategy({
+  callbackURL: config.googleAuth.callbackURL,
+  ralm: config.realm,
+  clientSecret: config.googleAuth.clientSecret,
+  clientID: config.googleAuth.clientID
+  },
+  function(accessToken, refreshToken, profile, done) {
+    console.log(profile);
+
+    done(null,profile);
+  }
+));
+
+//google auth lõpp
+
+var sessionOpt = {
+  secret: config.secret,
+  resave: false,
+  saveUnitialized: true,
+  cookie:{httpOnly:true, maxAge: 2419200000}
+};
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlexcoded({extended:true}));
+app.use(cookieParser(config.secret));
+app.use(session(sessionOpt));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/api/hello', function(req, res){
-  console.log('somebody visited /api/hello');
-  res.json({hello:'hello '});
+//mis juhtub, kui keegi tuleb
+app.get('/api/auth/google',
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/userinfo.profile',
+                                            'https://www.googleapis.com/auth/userinfo.email']}));
 
+app.get('/api/oauth2callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+//'mis juhtub' lõpp
+
+app.get('/api/me', function(req, res){
+  
 });
 
 var server = app.listen(3000, function(){
