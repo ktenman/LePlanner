a2e39266af800d799f8838fead603d8dc3b16ac6
vlad987 <minajev@tlu.ee>
2015-06-10 12:19:30 +0300

    added mongodb

--- App/controllers.js
+++ App/controllers.js
@@ -1,26 +1,28 @@
 var leplannerControllers = angular.module('leplannerControllers', []);
 
-leplannerControllers.controller('MainCtrl', [
-  '$scope',
-  '$http',
-  function($scope, $http){
-    // user auth
-    $http.get('/api/me').
-    success(function(data, status, headers, config){
-      console.log('User logged in');
-      $scope.user = data.displayName;
-      $scope.provider = data.provider;
-      console.log(data);
-    }).
-    error(function(data, status, headers, config){
-      console.log(data);
-    });
-  }
+leplannerControllers.controller('MainCtrl',[
+    '$scope',
+    '$http',
+    function($scope,$http){
+
+      // user auth
+      $http.get('/api/me').
+      success(function(data,status,headers,config){
+        console.log(data);
+        $scope.user = data.first_name;
+      }).
+      error(function(data,status,headers,config){
+        console.log(data);
+      });
+
+    }
 ]);
 
-leplannerControllers.controller('homeCtrl', [
-  '$scope',
-  function($scope){
-    $scope.message = 'Tere';
-  }
+leplannerControllers.controller('homeCtrl',[
+    '$scope',
+    function($scope){
+
+      $scope.message = 'Tere';
+
+    }
 ]);
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// server
 var config = require('./config/config');
+
 var express = require('express');
 var mongoose = require('mongoose');
 var passport = require('passport');
@@ -7,41 +7,71 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 
+mongoose.connect(config.db, function(err){
+  if(err) throw err;
+  console.log('successfully connected to Mongo db');
+});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, db.error));
+
+var User = require('./models/user');
+
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-var FacebookStrategy = require('passport-facebook').Strategy;
-
-// Facebook Auth
-passport.use(new FacebookStrategy({
-    clientID: config.facebookAuth.clientID,
-    clientSecret: config.facebookAuth.clientSecret,
-    callbackURL: config.facebookAuth.callbackURL
-  },
-  function(accessToken, refreshToken, profile, done){
-    console.log(profile);
-    done(null, profile);
-  }
-));
-
-// Google Auth
 passport.use(new GoogleStrategy({
-
     callbackURL: config.googleAuth.callbackURL,
     realm: config.realm,
     clientSecret: config.googleAuth.clientSecret,
     clientID: config.googleAuth.clientID
   },
-  function(accesstoken, refreshToken, profile, done) {
-    console.log(profile);
-    done(null, profile);
-  }
+  function(accessToken, refreshToken, profile, done) {
+    //console.log(profile);
+    console.log('logged in successfully');
 
+    var new_user = new User({
+      first_name: profile.name.givenName,
+      last_name: profile.name.familyName,
+      email: profile.emails[0].value,
+      google: {
+        id: profile.id,
+        email: profile.emails[0].value,
+      }
+    });
+
+    User.findOne({ email: new_user.email }, function(err, user){
+      if(err) {return done(err); }
+      if(!user){
+
+        new_user.save(function(err,user){
+          if(err) {return done(err); }
+
+          console.log('created new user with id: '+user._id);
+          done(null,user);
+        });
+
+      }else{
+        console.log('got user from db with id: '+user._id);
+        done(null,user);
+
+      }
+
+    });
+
+
+  }
 ));
 passport.serializeUser(function(user, done) {
-done(null, user);
+  done(null, user.id);
 });
 
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user){
+    if(err) {return res.json({error: err}); }
+    done(err, user);
+
+  });
+
+
 });
 
 var sessionOpt = {
@@ -59,23 +89,14 @@ app.use(cookieParser(config.secret));
 app.use(session(sessionOpt));
 app.use(passport.initialize());
 app.use(passport.session());
-// FACEBOOK AUTH
-app.get('/api/auth/facebook',
-  passport.authenticate('facebook', function(req, res){
-    // The request will be redirected to Facebook for authentication, so this
-    // function will not be called.
-  }));
 
-app.get('/api/auth/facebook/callback',
-  passport.authenticate('facebook', { failureRedirect: '/#/login'}),
-  function(req, res){
-    res.redirect('/#/');
-  });
 
-//  GOOGLE AUTH
+/* GOOGLE AUTH */
 app.get('/api/auth/google',
-  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/userinfo.profile',
-  'https://www.googleapis.com/auth/userinfo.email'] }));
+  passport.authenticate('google', { scope: [
+    'https://www.googleapis.com/auth/userinfo.profile',
+    'https://www.googleapis.com/auth/userinfo.email'
+  ]}));
 
 app.get('/api/oauth2callback',
   passport.authenticate('google', { failureRedirect: '/#/login' }),
@@ -86,25 +107,39 @@ app.get('/api/oauth2callback',
 
 var auth = function(req, res, next){
   if(!req.isAuthenticated()){
-    res.status(401).send({error:'unauthorized'});
+    res.status(401).send({error: 'unauthorized'});
   }else{
     next();
   }
 };
 
 app.get('/api/me', auth, function(req, res){
-  return res.json(req.session.passport.user);
+  //req.session.passport.user = [serializeUser ==> user.id ]
+  User.findById(req.session.passport.user, function(err, user){
+    if(err) {return res.json({error: err}); }
+    if(user){
+      return res.json(user);
+    }else{
+      return res.json({error: "not logged in"});
+    }
+
+  });
+
 });
 
 app.get('/api/logout', auth, function(req, res){
   console.log('logged out');
-  req.logout();
+  req.logOut();
   //res.status(200).send({success: 'success'});
   res.redirect('/#/');
 });
 
+
 var server = app.listen(config.port, function(){
+
   var host = server.address().address;
   var port = server.address().port;
+
   console.log('Server app running at http://%s:%s', host, port);
+
 });
