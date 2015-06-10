1789ef28ed72daa8b7f97a6d4259ba9ae1a834d5
Andre <andrep@tlu.ee>
2015-06-10 09:45:45 +0300

    app folder

---
+++ app/app.js
@@ -0,0 +1,17 @@
+var leplannerApp = angular.module('leplannerApp', [
+  'ngResource', 'ngRoute', 'leplannerControllers'
+]);
+
+leplannerApp.config(['$routeProvider', '$locationProvider', '$resourceProvider',
+function($routeProvider, $locationProvider, $resourceProvider){
+  $routeProvider
+  .when('/',
+    {templateUrl:'/views/home.html',
+    controller: 'homeCtrl'})
+  .when('/login',
+    {templateUrl:'/views/login.html',
+    controller: 'loginCtrl'})
+  .otherwise(
+    {redirectTo:'/'});
+
+}]);
