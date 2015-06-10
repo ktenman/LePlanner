adbbf82becdae9c610c5a4c4b2f6c8c25bf23abd
vlad987 <minajev@tlu.ee>
2015-06-10 10:32:44 +0300

    .

--- App/controllers.js
+++ App/controllers.js
@@ -1,33 +1,24 @@
 var leplannerControllers = angular.module('leplannerControllers', []);
 
-
-leplannerControllers.controller('MainCtrl',[
-    '$scope',
-    '$http',
-    function($scope, $http){
-
-      //user auth
-      $http.get('/api/me').
-      success(function(data, status, headers, config){
-        console.log('user logged in');
-
-        $scope.user = data.displayName;
-      }).
-      error(function(data, status, heades, config){
-        console.log(data);
-      });
-    }
-]);
-
-
-leplannerControllers.controller('homeCtrl',[
+leplannerControllers.controller('MainCtrl', [
   '$scope',
-  function($scope){
-    $scope.message = 'Tere';
+  '$http',
+  function($scope, $http){
+    // user auth
+    $http.get('/api/me').
+    success(function(data, status, headers, config){
+      console.log('User logged in');
+      $scope.user = data.displayName;
+      $scope.provider = data.provider;
+      console.log(data);
+    }).
+    error(function(data, status, headers, config){
+      console.log(data);
+    });
   }
 ]);
 
-leplannerControllers.controller('loginCtrl',[
+leplannerControllers.controller('homeCtrl', [
   '$scope',
   function($scope){
     $scope.message = 'Tere';
