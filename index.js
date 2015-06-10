b1f4015a7b1ac7fdd85abdc8029d06051aa1c2d4
elinda123 <elinda.tragel@gmail.com>
2015-06-10 09:17:06 +0300

    fb

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+// server
 var config = require('./config/config');
-
 var express = require('express');
 var mongoose = require('mongoose');
 var passport = require('passport');
@@ -8,20 +8,36 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+// Facebook Auth
+passport.use(new FacebookStrategy({
+    clientID: config.facebookAuth.clientID,
+    clientSecret: config.facebookAuth.clientSecret,
+    callbackURL: config.facebookAuth.callbackURL
+  },
+  function(accessToken, refreshToken, profile, done){
+    console.log(profile);
+    done(null, profile);
+  }
+));
+
+// Google Auth
 passport.use(new GoogleStrategy({
+
     callbackURL: config.googleAuth.callbackURL,
     realm: config.realm,
     clientSecret: config.googleAuth.clientSecret,
     clientID: config.googleAuth.clientID
   },
-  function(accessToken, refreshToken, profile, done) {
-    //console.log(profile);
-    console.log('logged in successfully');
-    done(null,profile);
+  function(accesstoken, refreshToken, profile, done) {
+    console.log(profile);
+    done(null, profile);
   }
+
 ));
 passport.serializeUser(function(user, done) {
-  done(null, user);
+done(null, user);
 });
 
 passport.deserializeUser(function(obj, done) {
@@ -43,14 +59,23 @@ app.use(cookieParser(config.secret));
 app.use(session(sessionOpt));
 app.use(passport.initialize());
 app.use(passport.session());
+// FACEBOOK AUTH
+app.get('/api/auth/facebook',
+  passport.authenticate('facebook', function(req, res){
+    // The request will be redirected to Facebook for authentication, so this
+    // function will not be called.
+  }));
 
+app.get('/api/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/#/login'}),
+  function(req, res){
+    res.redirect('/#/');
+  });
 
-/* GOOGLE AUTH */
+//  GOOGLE AUTH
 app.get('/api/auth/google',
-  passport.authenticate('google', { scope: [
-    'https://www.googleapis.com/auth/userinfo.profile',
-    'https://www.googleapis.com/auth/userinfo.email'
-  ]}));
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/userinfo.profile',
+  'https://www.googleapis.com/auth/userinfo.email'] }));
 
 app.get('/api/oauth2callback',
   passport.authenticate('google', { failureRedirect: '/#/login' }),
@@ -61,7 +86,7 @@ app.get('/api/oauth2callback',
 
 var auth = function(req, res, next){
   if(!req.isAuthenticated()){
-    res.status(401).send({error: 'unauthorized'});
+    res.status(401).send({error:'unauthorized'});
   }else{
     next();
   }
@@ -71,18 +96,14 @@ app.get('/api/me', auth, function(req, res){
   return res.json(req.session.passport.user);
 });
 
-app.get('/api/logout', function(req, res){
+app.get('/api/logout', auth, function(req, res){
   console.log('logged out');
-  req.logOut();
-  res.status(200).send({success: "success"});
+  req.logout();
+  res.status(200).send({success: 'success'});
 });
 
-
 var server = app.listen(config.port, function(){
-
   var host = server.address().address;
   var port = server.address().port;
-
   console.log('Server app running at http://%s:%s', host, port);
-
 });
