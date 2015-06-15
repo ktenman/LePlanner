3073962392605be6fe718ada48aeacf302bc0523
Andre <andrep@tlu.ee>
2015-06-15 13:18:48 +0300

    Added Technical table

---
+++ models/technical.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+var TechnicalSchema = new Schema({
+  tech: { type: String}
+});
+
+module.exports = mongoose.model('Technical', TechnicalSchema);
