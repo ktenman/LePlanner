25c750f38231374b4b14d624f54da28b0944dbe5
Andre <andrep@tlu.ee>
2015-06-15 13:28:06 +0300

    Added all tables made by Marten

---
+++ models/stage.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+var StageSchema = new Schema({
+  stage: { type: String}
+});
+
+module.exports = mongoose.model('Stage', StageSchema);
